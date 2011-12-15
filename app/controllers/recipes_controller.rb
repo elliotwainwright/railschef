1dfa7157e9ac1415689ca3a83186d74912387f7b
elliotwainwright <elliot_wainwright@hotmail.com>
2011-12-15 16:20:01 +0000

    Fully Working Basic App With Forms Styled

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,21 +15,28 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
   
-  def update
-    @recipe = Recipe.find(params[:id])
-    @recipe.update_attributes(params[:recipe])
-    redirect_to @recipe
+  def create
+    @recipe = Recipe.create(params[:recipe])
+    flash[:notice] = "Recipe Created"
+    redirect_to root_url
   end
   
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.save
+      @recipe.update_attributes(params[:recipe])
+      flash[:notice] = "Recipe Updated"
+      redirect_to @recipe
+    else
+      flash[:notice] = "Epic Fail"
+      redirect_to edit_recipe_path
+    end
+  end
+
   def destroy
       @recipe = Recipe.find(params[:id])
       @recipe.delete
       redirect_to recipes_path
-  end
-  
-  def create
-    @recipe = Recipe.create(params[:recipe])
-    redirect_to root_url
   end
   
 end
